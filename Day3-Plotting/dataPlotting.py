0860b3dfdbaa29a6f56e86ceed3fe805386ecff4
Joshua Szekely <jeszekely@gmail.com>
2015-02-10 17:28:53 -0600

    First round of corrections to fitting and baseline subtraction

--- Day3-Plotting/dataPlotting.py
+++ Day3-Plotting/dataPlotting.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python2.7
+"""
+Before proceeding, change to the proper directory
+Change the plotting format to SVG in preferences
+"""
 
-import glob, os, operator
+#!/usr/bin/env python2.7
+# Change the directory before using this script
+
+import glob, os, operator, sys
 import numpy as np
 import scipy.signal
 import matplotlib.pyplot as plt
@@ -8,7 +14,10 @@ import matplotlib.pyplot as plt
 # Let's use our custom class to store our data
 from SpectrumClass import Spectrum
 
-# Change to the proper directory
+# If you need to change the plot size, use this
+# plt.gcf().set_size_inches(8.0,6.0)
+
+rootDir = os.getcwd() # Store current directory so we can change back later
 os.chdir("R6G_CWUHVTERS")
 
 # Import the x-axis
@@ -18,120 +27,138 @@ xData = np.genfromtxt("032614_Ne_pt1s_20_poly3cal_1.txt",usecols=(0))
 allFiles = glob.glob('*cent567_VB1V*.txt')
 spectra  = []
 for f in allFiles:
-    spectra.append(Spectrum(f))
+    spectra.append(Spectrum(f)) # Change name?
+
+# Now that the data is loaded, let's move back to the root directory
+os.chdir(rootDir)
 
 # Now let's sort them based on the appropriate number
 spectra.sort(key=operator.attrgetter('number'))
 
 # Data is now fully imported!
 # Let's make a basic plot of one of the data sets
-
 plt.plot(xData, spectra[30].data)
 plt.title("Raw Data")
 plt.show()
 
 yData = spectra[30].data
 
-######################
-# Noise  Correction
-######################
+##########################################
+# Noise  Correction and baseline fitting
+##########################################
 
 # Median Line filter
-
-yData_corr = scipy.signal.medfilt(yData, 5)
-plt.plot(xData,yData_corr)
+yData_med = scipy.signal.medfilt(yData, 5)
+plt.plot(xData,yData_med)
 plt.title("Median Line Filter")
+plt.show()
 
-# Savgol-Golay filter
-
+# Savitsky-Golay filter
 yData_sav = scipy.signal.savgol_filter(yData, 5, 1)
 plt.plot(xData,yData_sav)
 plt.title("Savitsky-Golay Filter")
 plt.show()
 
-# baseline subtraction
+# baseline fitting
+baseline = scipy.signal.savgol_filter(yData, 51, 2) #This is a first approximation to the baseline
 
-median   = np.median(yData)
-baseline = scipy.signal.savgol_filter(yData, 299, 2) #This is a first approximation to the baseline
+plt.plot(xData,baseline)
+plt.title("Crude Baseline")
+plt.show()
 
-diff  = np.abs(yData_sav - baseline)
-width = max(diff) - min(diff)
-bad   = diff/width > 0.4
+# Let's correct the baseline to make it a bit smoother
+for ii in range(50,300,30):
+    baseline = scipy.signal.savgol_filter(baseline, ii+1, 2)
+baseline = baseline-np.ones(len(baseline))*10.0 #Shift it down a little
 
-yData_base      = yData_sav.copy()
-yData_base[bad] = baseline[bad]
+plt.plot(xData,baseline, xData, yData)
+plt.title("Corrected Baseline")
+plt.show()
 
-# baseline = scipy.signal.savgol_filter(yData_base, 299, 2) #Correct Baseline
-
+# Finally subtract the baseline
 modData = yData_sav - baseline
 plt.plot(xData, modData, xData, yData-baseline)
 plt.title("Savitsky-Golay Filter, Baseline Subtracted")
 plt.show()
 
+
 ###################
 # Peak Finding
 ###################
 
-def test(xD,yD,num,num2,num3=10.0, num4=5, num5=4):
-    # plt.plot(xD,yD)
-    somePeaks  = scipy.signal.find_peaks_cwt(yD, np.arange(num,num2),noise_perc=num3)
-    otherPeaks = scipy.signal.argrelmax(yD,order = num4, mode="wrap")[0]
-    peaks = [p for p in somePeaks if set([]) != ( set(otherPeaks) & set(range(p-num5,p+num5)) ) ]
-    # plt.plot(xD[peaks],yD[peaks],'ro',markersize=10)
-    # plt.plot(xD[list(somePeaks)],yD[list(somePeaks)],'bs')
-    # plt.plot(xD[list(otherPeaks)],yD[list(otherPeaks)],'g^')
-    # plt.show()
-    return peaks
 
-#Still not the best for getting peaks
-peaks = test(xData, modData, 5, 10, 10, 10, 5)
+#peaks  = scipy.signal.find_peaks_cwt(modData, np.arange(1,5),noise_perc=10)
+
+# finds all local max in smoothed data set
+plt.gcf().set_size_inches(16.0,12.0)
+somePeaks = scipy.signal.argrelmax(modData,order = 30, mode="wrap")[0]
+
+#filter out those that are too small
+peaks = [p for p in somePeaks if modData[p] > 20]
+
 plt.plot(xData, modData)
 plt.plot(xData[peaks], modData[peaks],'ro')
 plt.show()
 
-def smooth(y, box_pts):
-    box = np.ones(box_pts)/box_pts
-    y_smooth = np.convolve(y, box, mode='same')
-    return y_smooth
+# Alternate smoothing function
+#def smooth(y, box_pts):
+#    box      = np.ones(box_pts)/box_pts
+#    y_smooth = np.convolve(y, box, mode='same')
+#    return y_smooth
 
 #Slightly different approach
-smoothModData = smooth(modData,50)
-peaks = test(xData, smoothModData, 5, 10, 10, 10, 5)
-plt.plot(xData, modData)
-plt.plot(xData[peaks], modData[peaks],'ro')
-plt.show()
+#smoothModData = smooth(modData,50)
+#plt.plot(xData, modData)
+#plt.plot(xData[peaks], modData[peaks],'ro')
+#plt.show()
 
-#################################
-# Curve Fitting
-#################################
+###################################################
+# Curve Fitting - one of the most requested topics
+###################################################
 
 from scipy.optimize import curve_fit
 
-def gaussian(x,a,b,c):
-    return a*np.exp(-1.0*pow(x-b,2)/(2.0*c*c))
+#lorenzians
+def gaussian(x,A,x0,s):
+    return a*np.exp(-1.0*pow(x-x0,2)/(2.0*s*s))
 
-peaks = [p for p in peaks if (p > 10 and p < len(xData)-10)]
+def lorentzian(x,A,G,x0):
+    return (A/np.pi)*(0.5*G)/(pow(x-x0,2) + pow(0.5*G,2))
 
+# Make array for reproduced data
 reprodData = np.zeros(len(xData))
 
 for peak in peaks:
-    width = 10
-    lowerInd = peak-width
-    upperInd = peak+width
-    xD = xData[lowerInd:upperInd]
-    yD = modData[lowerInd:upperInd]
-    aGuess = modData[peak]
-    bGuess = xData[peak]
-    cGuess = 0.5
-    popt,pconv = curve_fit(gaussian, xD, yD, p0=[aGuess,bGuess,cGuess])
+    width      = 10
+    lowerInd   = peak-width
+    upperInd   = peak+width
+    xD         = xData[lowerInd:upperInd]
+    yD         = modData[lowerInd:upperInd]
+    aGuess     = modData[peak] # Peak Height
+    bGuess     = xData[peak]   # Peak Location
+    cGuess     = 0.50          # Peak width
+    try:
+        popt,pconv = curve_fit(lorentzian, xD, yD, p0=[aGuess,bGuess,cGuess])
+    except:
+        print "Error fitting peak at x =", xData[peak]
+        next
     print popt
     for ii in range(len(xData)):
-        reprodData[ii] += gaussian(xData[ii],popt[0],popt[1],popt[2])
+        reprodData[ii] += lorentzian(xData[ii],popt[0],popt[1],popt[2])
 
+plt.gcf().set_size_inches(16.0,12.0)
 plt.plot(xData, modData, xData,reprodData)
 plt.show()
 
+plt.gcf().set_size_inches(16.0,12.0)
+plt.plot(xData, yData,)
+plt.plot(xData,reprodData+baseline,linewidth=2.0,color="black")
+plt.title("Fully fitted spectrum")
+plt.show()
 
+#Finally, a fully reconstructed spectrum
+
+#Convert to 2D array of spectra
 
 #####################
 # A gift from Marty
@@ -148,12 +175,12 @@ def smooth(x,window_len=10,window='hanning'):
         raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
     x0 = x[window_len-1:0:-1]
     xn = x[-1:-window_len:-1]
-    s = numpy.r_[x0,x,xn]
+    s  = numpy.r_[x0,x,xn]
     if window == 'flat': #moving average
         w = numpy.ones(window_len,'d')
     else:
         w = eval('numpy.'+window+'(window_len)')
-    y = numpy.convolve(w/w.sum(),s,mode='valid')
+    y   = numpy.convolve(w/w.sum(),s,mode='valid')
     lx0 = len(x0)
     if lx0 % 2 != 0:
         lx0=lx0+1
