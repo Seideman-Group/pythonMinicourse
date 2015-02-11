ce09ae2ba52e0e201f19a8f035edff35d7ca8099
Thomas Purcell <tap620@Tom-Work.attlocal.net>
2015-02-11 16:27:15 -0600

    better Lorentzian

--- Day1-Basics/Lorentzian.py
+++ Day1-Basics/Lorentzian.py
@@ -1,7 +1,7 @@
 import math
 
 def lorenz(x, center, width, peakhieght):
-    return 1.0/math.pi * (0.5*width) / ((x-center)**2 + (0.5*width)**2)
+    return 1.0/math.pi * (0.5*width) / ((x-center)**2 + (0.5*width)**2) * peakhieght*(width*math.pi/2)
 
 def makeLorenz(center, width, peakhieght, tol, step):
     x = center
@@ -11,9 +11,9 @@ def makeLorenz(center, width, peakhieght, tol, step):
         lor.append(lorenz(x,center, width, peakhieght))
         x = x + step
     lor_copy = []
-    lor_copy.extend(lor)
+    lor_copy[:] = lor[:]
     lor.reverse()
-    lor.extend(lor_copy)
+    lor[len(lor)-1:] = lor_copy[:]
     x = [i*step - step*(len(lor)-1)/2.0 + center for i in range(len(lor)) ]
     return x, lor
 
@@ -23,9 +23,10 @@ def integrate(lor, step):
         s = s + num*step
     return s
 
-
 import matplotlib.pyplot as plt
-x, lor = makeLorenz(1.0,1.0,1.0,1e-5,.01)
-print integrate(lor, 0.01)
+x, lor = makeLorenz(1.0,2.0,3.0,1e-5,.001)
+print integrate(lor, 0.001)
+print lor[len(lor)/2-5:len(lor)/2+5]
 plt.plot(x, lor)
+plt.axis([-4,6,0,3.1])
 plt.show()
