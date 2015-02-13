3d996f04e5802810f6692844bdc48561eb5c13b1
Tom Purcell <thomaspurcell2018@u.northwestern.edu>
2015-02-12 18:44:49 -0600

    Changed Lecture format to building up a lorentzian list function

--- Day1-Basics/Lorentzian.py
+++ Day1-Basics/Lorentzian.py
@@ -1,4 +1,17 @@
 import math
+#previous work
+# Lorentz function is defined defined as 1/pi * A * 1/2Gam/((x-x0)^2 +(1/2Gam)**2)
+# write a for loop that can get the values of a lorentzian centered at 5.0 and a width of 2.0 and A = pi from 0 to 10 with a numerical step of 0.01
+#for i in range(10*100+1):
+#    print str(i*0.01) + " \t " + str(1.0/((i*0.01-5.0)**2 + (1.0)**2 ))
+
+# Using the definition of the Lortenzian you did in the last exercise to calculate the area under a Lorentzian while its value is >= 1e-3
+#x = 5.0
+#s = 0.0
+#while(1.0/((x-5.0)**2 + (1.0)**2) >= 1e-8):
+#    s = s + 1.0/((x-5.0)**2 + (1.0)**2) * 0.001
+#    x = x + 0.001
+#print s * 2 - 0.001
 
 def lorenz(x, center, width, peakhieght):
     return 1.0/math.pi * (0.5*width) / ((x-center)**2 + (0.5*width)**2) * peakhieght*(width*math.pi/2)
