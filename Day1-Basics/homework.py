180e991f44f13798a0f15920006b4f54d94e138d
Tom Purcell <thomaspurcell2018@u.northwestern.edu>
2015-02-16 15:08:51 -0600

    Modfied my files, added a homework.py and added a bit on functions to the reference.py

---
+++ Day1-Basics/homework.py
@@ -0,0 +1,7 @@
+# Problem 1: Use the string manipulations to replace Name with any Name
+s = "Hello Name I hope you are having a great day!"
+
+# Problem 2: Replace the strings in the following list with the approriate numbers, Remove all non number string elements
+l = [0, 1, 2, "3", "4", 5, 6, "Cats", 7, '8', 9, "puppy"]
+
+# Problem 3: Using what you learened from the Lorentz storage and functions, make multiple functions that will generate a list of Lorentz function values for a set of upper and lower bounds, and check if that approx is good
