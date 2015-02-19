e2d4149300d74d173e7b058a4cbfe4c656201d78
Joshua Szekely <jeszekely@gmail.com>
2015-02-18 21:41:49 -0600

    Added final file modifications which will be given to the class before the Day 3 tutorial

--- Day3-Plotting/dataPlotting.py
+++ Day3-Plotting/dataPlotting.py
@@ -1,158 +1,149 @@
 """
-Before proceeding, change to the proper directory
-Change the plotting format to SVG in preferences
+Objectives for Day 3 Part 1:
+    - Import spectral data into python using classes
+    - Make a really simple plot
+    - Data manipulations
+        - Smoothing
+        - Baseline correction
+        - Curve fitting
+    - Really simple heat map
+
+Note: Change the plotting format to SVG in preferences
+Note: Also, Change the directory before using this script
 """
 
-#!/usr/bin/env python2.7
-# Change the directory before using this script
+# Import all the modules
 
-import glob, os, operator
-import numpy as np
-import scipy.signal
-import matplotlib.pyplot as plt
 
-# Let's use our custom class to store our data
-from SpectrumClass import Spectrum
 
-# If you need to change the plot size, use this
-# plt.gcf().set_size_inches(8.0,6.0)
-
-rootDir = os.getcwd() # Store current directory so we can change back later
-os.chdir("R6G_CWUHVTERS")
 
 # Import the x-axis
-xData = np.genfromtxt("032614_Ne_pt1s_20_poly3cal_1.txt",usecols=(0))
-
 # Load all spectra into an array, first get all file names, then create Spectrum objects
-allFiles = glob.glob('*cent567_VB1V*.txt')
-spectra  = []
-for f in allFiles:
-    spectra.append(Spectrum(f)) # Change name?
 
-# Now that the data is loaded, let's move back to the root directory
-os.chdir(rootDir)
+
+
 
 # Let's sort them based on the appropriate number
-spectra.sort(key=operator.attrgetter('number'))
 
-# Data is now fully imported!
+
+
+
 # Let's make a basic plot of one of the data sets
-plt.plot(xData, spectra[35].data)
-plt.title("Raw Data")
-plt.show()
 
-yData = spectra[50].data
+
+
+# Let's just work with one piece for now
+
 
 ##########################################
 # Noise  Correction and baseline fitting
 ##########################################
 
-# Median Line filter
-yData_med = scipy.signal.medfilt(yData, 5)
-plt.plot(xData,yData_med)
-plt.title("Median Line Filter")
-plt.show()
+#import the scipy signal library
+
+
+# Median filter
+
+
+
+# plt.plot(xData,yData_med)
+# plt.title("Median Line Filter")
+# plt.show()
 
 # Savitsky-Golay filter
-yData_sav = scipy.signal.savgol_filter(yData, 5, 1)
-plt.plot(xData,yData_sav)
-plt.title("Savitsky-Golay Filter")
-plt.show()
+
+
+
+# plt.plot(xData,yData_sav)
+# plt.title("Savitsky-Golay Filter")
+# plt.show()
 
 # baseline fitting
-baseline = scipy.signal.savgol_filter(yData, 51, 2) #This is a first approximation to the baseline
 
-plt.plot(xData,baseline)
-plt.title("Crude Baseline")
-plt.show()
 
-# Let's correct the baseline to make it a bit smoother
-for ii in range(50,300,30):
-    baseline = scipy.signal.savgol_filter(baseline, ii+1, 2)
-baseline = baseline-10.0 # Shift it down a little
 
-plt.plot(xData,baseline, xData, yData)
-plt.title("Corrected Baseline")
-plt.show()
+
+
+#plt.plot(xData,baseline)
+#plt.title("Crude Baseline")
+#plt.show()
+
+
+# Now let's correct the baseline to make it a bit smoother
+
+
+
+
+
+#plt.plot(xData,baseline, xData, yData)
+#plt.title("Corrected Baseline")
+#plt.show()
 
 # Finally subtract the baseline
-modData = yData_sav - baseline
-plt.plot(xData, modData, xData, yData-baseline)
-plt.title("Savitsky-Golay Filter, Baseline Subtracted")
-plt.show()
 
+
+
+
+#plt.plot(xData, modData, xData, yData-baseline)
+#plt.title("Savitsky-Golay Filter, Baseline Subtracted")
+#plt.show()
 
 ############################################################
 # Peak Finding - Check scipy signal library for more options
 ############################################################
 
 # finds all local max in smoothed data set
-plt.gcf().set_size_inches(16.0,12.0)
-somePeaks = scipy.signal.argrelmax(modData,order = 30, mode="wrap")[0]
+# filter out those that are too small
 
-#filter out those that are too small
-peaks = [p for p in somePeaks if modData[p] > 20]
 
-plt.plot(xData, modData)
-plt.plot(xData[peaks], modData[peaks],'ro')
-plt.show()
+
+
+
+
+
+
+
+#plt.gcf().set_size_inches(16.0,12.0)
+#plt.plot(xData, modData)
+#plt.plot(xData[peaks], modData[peaks],'ro')
+#plt.show()
 
 ###################################################
 # Curve Fitting - one of the most requested topics
 ###################################################
 
-from scipy.optimize import curve_fit
 
-def gaussian(x,A,x0,s):
-    return A*np.exp(-1.0*pow(x-x0,2)/(2.0*s*s))
 
-def lorentzian(x,A,G,x0):
-    return (A/np.pi)*(0.5*G)/(pow(x-x0,2) + pow(0.5*G,2))
 
-# Make array for reproduced data
-reprodData = np.zeros(len(xData))
 
-for peak in peaks:
-    width      = 10
-    lowerInd   = peak-width
-    upperInd   = peak+width
-    xD         = xData[lowerInd:upperInd]
-    yD         = modData[lowerInd:upperInd]
-    aGuess     = modData[peak] # Peak Height
-    bGuess     = xData[peak]   # Peak Location
-    cGuess     = 0.50          # Peak width
-    try:
-        popt,pconv = curve_fit(lorentzian, xD, yD, p0=[aGuess,bGuess,cGuess])
-        print popt
-        for ii in range(len(xData)):
-            reprodData[ii] += lorentzian(xData[ii],popt[0],popt[1],popt[2])
-    except:
-        print "Error fitting peak at x =", xData[peak]
-        next
-    
 
-plt.gcf().set_size_inches(16.0,12.0)
-plt.plot(xData, modData, xData,reprodData)
-plt.show()
 
-plt.gcf().set_size_inches(16.0,12.0)
-plt.plot(xData, yData,)
-plt.plot(xData,reprodData+baseline,linewidth=2.0,color="black")
-plt.title("Fully fitted spectrum")
-plt.show()
 
-# Finally, a fully reconstructed spectrum!
 
-# In preparation for the next section...
+#plt.gcf().set_size_inches(16.0,12.0)
+#plt.plot(xData, modData)
+#plt.plot(xData, reprodData)
+#plt.show()
+
+#plt.gcf().set_size_inches(16.0,12.0)
+#plt.plot(xData, yData,)
+#plt.plot(xData,reprodData+baseline,linewidth=2.0,color="black")
+#plt.title("Fully fitted spectrum")
+#plt.show()
+
 # Convert to 2D array of spectra
 
-data2D = np.zeros([len(spectra),len(spectra[0].data)])
 
-for ii in range(len(spectra)):
-    data2D[ii] = spectra[ii].data
 
-plt.pcolormesh(xData, np.arange(0,len(spectra)), data2D)
-plt.axis([min(xData),max(xData),0, len(spectra)])
-plt.xlabel(r'Wavenumber / cm$^{-1}$')
-plt.ylabel("Number")
-plt.show()
+
+
+
+
+
+
+
+
+#plt.axis([min(xData),max(xData),0, len(spectra)])
+#plt.xlabel(r'Wavenumber / cm$^{-1}$')
+#plt.ylabel("Number")
+#plt.show()
