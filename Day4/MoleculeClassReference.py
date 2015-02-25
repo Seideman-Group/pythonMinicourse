d20987a735adc7cc2acac3d4f9ae553e97453d8f
Joshua Szekely <jeszekely@gmail.com>
2015-02-25 13:30:07 -0600

    Finished Molecule class reference

--- Day4/MoleculeClassReference.py
+++ Day4/MoleculeClassReference.py
@@ -1,41 +1,22 @@
 """
-Make class for atoms and molecules
-Atoms: storage class (i.e. no functions other than __init__)
-    coordinates
-    element name
-    mass of atom (use atomDict.py to get this info)
-
-Molecules: constructs a group of atoms from a .out file
-    data:
-        number of atoms
-        List of Atoms (coordinates and names) - use the Atom class you already made
-        Energy
-        Source file name
-        name of molecule
-    functions:
-        __init__(self, filename, molname):
-            fills data
-                get list of atoms, place in array
-                extract coordinate information
-                grab energy for the converged calculation
-
-        centerOfMass(self):
-            calculates the center of mass
-
-        distance(self,n,m):
-            calculate the distance between the nth and mth atom
-
-        printCoords(self):
-            nicely print the coordinates of all the atoms in the molecule
-
-        plotEnergyConvergence(self):
+Objective: Make class for atoms and molecules which extracts information from a QChem output file
+Steps:
+    Examine the .out files to understand the format and identify where the information you need is located
+    Write some pseudo code
+    Possible outlines for these classes are provided below
 """
-
-from atomDict import elementMasses
+from atomDict import elementMasses #We've provided a resource to get all the masses of elements you need
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Atom:
+    """
+    Atoms: storage class (i.e. no functions other than __init__)
+        coordinates
+        element name
+        mass of atom (use atomDict.py to get this info)
+    """
     def __init__(self, name, X, Y, Z):
         self.x       = X
         self.y       = Y
@@ -43,7 +24,36 @@ class Atom:
         self.element = name.lower()
         self.mass    = elementMasses[self.element]
 
+
 class Molecule:
+    """
+    Molecule: constructs a group of atoms from a .out file
+        data:
+            number of atoms
+            List of Atoms (coordinates and names) - use the Atom class you already made
+            Energy
+            Source file name
+            name of molecule
+        functions:
+            __init__(self, filename, molname):
+                fills data
+                    get list of atoms, place in array
+                    extract coordinate information
+                    grab energy for the converged calculation
+
+            centerOfMass(self):
+                calculates the center of mass
+
+            distance(self,n,m):
+                calculate the distance between the nth and mth atom
+
+            printCoords(self):
+                nicely print the coordinates of all the atoms in the molecule
+
+            plotEnergyConvergence(self):
+                plot the energy vs computational cycle
+                remember, exclude the last energy from the single point calculation
+    """
     def __init__(self, filename, moleculeName):
         if not os.path.isfile(filename):
             raise Exception("Molecule object needs a valid filename")
@@ -59,7 +69,7 @@ class Molecule:
         outFile.close()
 
 # Get the number of atoms
-        ll = 0
+        ll = 0 # ll is the index in the array where the atom coordinates are located
         for ii,line in enumerate(lines):
             if "NAtoms," in line:
                 self.nAtoms = int(lines[ii+1].split()[0])
@@ -76,7 +86,7 @@ class Molecule:
             x = float(atmLine[2])
             y = float(atmLine[3])
             z = float(atmLine[4])
-            self.atoms.append(Atom(name,x,y,z))
+            self.atoms.append( Atom(name,x,y,z) )
 
     def centerofMass(self):
         cenMass = np.zeros(3)
@@ -89,16 +99,45 @@ class Molecule:
         for ii in range(len(cenMass)):
             cenMass[ii] = cenMass[ii] / totalMass
         return cenMass
+
     def printCoords(self):
-        print "name\tX\tY\tZ"
+        print "name\tX\t\tY\t\tZ"
         for atm in self.atoms:
             print atm.element,'\t',atm.x,'\t',atm.y,'\t',atm.z
+            # print "%s\t%.6f\t%.6f\t%.6f" % (atm.element,atm.x,atm.y,atm.z) # Alternate printing method #1
+            # print "{name:^15}{x:>15}{y:>15}{z:>15}".format(name=atm.element,x=atm.x,y=atm.y,z=atm.z) # Alternate printing method #2
+
     def distance(self,n,m):
         return np.sqrt((self.atoms[n].x-self.atoms[m].x)**2 + (self.atoms[n].y-self.atoms[m].y)**2 + (self.atoms[n].z-self.atoms[m].z)**2)
+
+    def plotEnergyConvergence(self):
+        inFile = open(self.source)
+        lines = inFile.readlines()
+        inFile.close()
+        energies = np.array([float(line.split()[1]) for line in lines if("Convergence criterion met" in line)])
+        plt.plot(energies[:-1],'ro-')
+        plt.xlabel("Step")
+        plt.ylabel("Energy / au")
+        plt.title("Energy Convergence of " + self.name)
+        plt.show()
+
+
+# Test the class
 
 Mol = Molecule("CO.out","CO")
 for a in Mol.atoms:
     print a.z
 print Mol.centerofMass()
 print Mol.distance(0,1)
-print Mol.printCoords()
+
+Mol.printCoords()
+Mol.plotEnergyConvergence()
+
+Mol2 = Molecule("CHI3.out",r"CHI$_3$")
+for a in Mol2.atoms:
+    print a.z
+print Mol2.centerofMass()
+print Mol2.distance(0,1)
+
+Mol2.printCoords()
+Mol2.plotEnergyConvergence()
