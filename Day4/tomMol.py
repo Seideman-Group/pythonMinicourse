b7d22a07b44c446a0bd405a5ac5b6a5b55230baf
Tom Purcell <thomaspurcell2018@u.northwestern.edu>
2015-02-25 09:54:44 -0600

    improved files and added a center of mass calc

--- Day4/tomMol.py
+++ Day4/tomMol.py
@@ -12,35 +12,54 @@ class molecule:
         energy       = [float(line.split()[3]) for line in lines if("Final energy is " in line)]
         self.energy_ = energy[0]
         ll = 0
-        while( not ("$molecule" in lines[ll])):
-            ll = ll+1
-
-        ll = ll+2
-        while( not("$end" in lines[ll])):
-            # print lines[ll]
-            atmLine = lines[ll].split()
-            # print atmLine
-            # print ll
-            if(len(atmLine) > 1):
-                self.atoms_.append(atom(atmLine[0], float(atmLine[1]), float(atmLine[2]), float(atmLine[3])))
-            ll = ll +1
+        #while( not ("$molecule" in lines[ll])):
+        #    ll = ll+1
+        #ll = ll+2
+        #while( not("$end" in lines[ll])):
+        #    # print lines[ll]
+        #    atmLine = lines[ll].split()
+        #    # print atmLine
+        #    # print ll
+        #    if(len(atmLine) > 1):
+        #        self.atoms_.append(atom(atmLine[0], float(atmLine[1]), float(atmLine[2]), float(atmLine[3])))
+        #    ll = ll +1
 
         for ll in range(len(lines)):
             if("Coordinates (Angstroms)" in lines[ll]):
-                for ii in range(len(self.atoms_)):
-                    atmLine = lines[ll+ii+2].split()
-                    # print atmLine
-                    self.atoms_[ii].x_ = atmLine[2]
-                    self.atoms_[ii].y_ = atmLine[3]
-                    self.atoms_[ii].z_ = atmLine[4]
+                if (len(self.atoms_) == 0):
+                    print "Getting molecular coords"
+                    aa = 2
+                    while(not "Point Group:" in lines[ll+aa]):
+                        atmLine = lines[ll+aa].split()
+                        self.atoms_.append(atom(atmLine[1], float(atmLine[2]), float(atmLine[3]), float(atmLine[4])))
+                        aa = aa+1
+                else:                 
+                    for ii in range(len(self.atoms_)):
+                        atmLine = lines[ll+ii+2].split()
+                        # print atmLine
+                        self.atoms_[ii].x_ = float(atmLine[2])
+                        self.atoms_[ii].y_ = float(atmLine[3])
+                        self.atoms_[ii].z_ = float(atmLine[4])
+    def centerOfMass(self):
+        cenMass = [0.0,0.0,0.0]
+        mass    = 0.0
+        for atm in self.atoms_:
+            cenMass[0] = cenMass[0] + atm.x_ * atm.mass_
+            cenMass[1] = cenMass[1] + atm.y_ * atm.mass_
+            cenMass[2] = cenMass[2] + atm.z_ * atm.mass_
+            mass = mass + atm.mass_
+        for ii in range(len(cenMass)):
+            cenMass[ii] = cenMass[ii] / mass
+        return cenMass
 
 
-
-mol = molecule("CHI3.out")
+mol = molecule("CO.out")
 print mol.n_opt
 for atm in mol.atoms_:
     print atm.element_
+    print atm.mass_
     print atm.x_
     print atm.y_
     print atm.z_
     print "done"
+print mol.centerOfMass()
